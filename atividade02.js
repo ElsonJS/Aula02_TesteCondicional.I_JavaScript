86101ebc97d0fb46c24efef40176383786d1ad67
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-08-04 20:03:43 -0300

    Update and rename atividade02.html to atividade02.js

---
+++ atividade02.js
@@ -0,0 +1,10 @@
+<script>
+   //Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo
+   var n1=prompt("Digite um numero: ");
+  
+   if(n1 > 0){
+      document.write("<b>O numero informado é Positivo!</b>");
+   }else{
+      document.write("<b>O numero informado é Negativo!</b>"); 
+   }
+</script>
